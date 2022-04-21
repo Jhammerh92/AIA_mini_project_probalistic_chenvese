52bc0b2864612074acfb0e70c5be16f3443c02b0
Jakob Hedemann <jhammerh92@gmail.com>
2022-04-21 18:01:22 +0200

    Snake with GMM working in histograms

--- chan_vese_histogram.py
+++ chan_vese_histogram.py
@@ -6,13 +6,23 @@ import cv2
 
 
 
-im = AiA.imread("probabilistic_data/overlap_test.png")
-
-AiA.imshow(im)
+# im = AiA.imread("probabilistic_data/overlap_test.png")
+im = AiA.imread("probabilistic_data/plante_downsize.jpg", as_grey = True)
+im = im*255
+# AiA.imshow(im)
 
 snake = snek.snake(100, im, tau=2.5, alpha=0.5,beta=0.02)
 
-snake.converge_to_shape(ax=None, conv_lim_pix=0.01, show_normals=True)
+# snake.show() 
+snake.init_EM_gaussians(peaks=3, std=35)
+snake.plot_histograms(with_gaussians=True)
+snake.EM_converge(iter=25)
+snake.plot_histograms(with_gaussians=True)
+
+plt.show()
+
+
+# snake.converge_to_shape(ax=None, conv_lim_pix=0.01, show_normals=True)
 
 
 # plt.show()
