ee211e5340b36c54b15b923b4056be36d6742ce9
Jakob Hedemann <jhammerh92@gmail.com>
2022-04-21 13:41:14 +0200

    new snake

--- chan_vese_histogram.py
+++ chan_vese_histogram.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import AiA
+import snake as snek
 import cv2
 
 
@@ -9,4 +10,9 @@ im = AiA.imread("probabilistic_data/overlap_test.png")
 
 AiA.imshow(im)
 
-plt.show()
+snake = snek.snake(100, im, tau=2.5, alpha=0.5,beta=0.02)
+
+snake.converge_to_shape(ax=None, conv_lim_pix=0.01, show_normals=True)
+
+
+# plt.show()
