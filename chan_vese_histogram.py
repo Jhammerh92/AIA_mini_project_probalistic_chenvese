ab3fedf343d2f440c025900a6d74231db999d20e
Jakob Hedemann <jhammerh92@gmail.com>
2022-05-01 19:11:47 +0200

    j

--- chan_vese_histogram.py
+++ chan_vese_histogram.py
@@ -11,10 +11,10 @@ import cv2
 # im = AiA.imread("probabilistic_data/simple_test.png", as_type=True,load_type=np.uint8)
 # im = AiA.imread("probabilistic_data/randen15.png", as_type=True,load_type=np.uint8)
 # im = AiA.imread("probabilistic_data/test_C.png", as_type=True,load_type=np.uint8)
-# im = AiA.imread("probabilistic_data/12003.jpg")
+im = AiA.imread("probabilistic_data/12003.jpg")
 # im = AiA.imread("probabilistic_data/105053.jpg")
 # im = plt.imread("probabilistic_data/108073.jpg")
-im = plt.imread("probabilistic_data/124084.jpg")
+# im = plt.imread("probabilistic_data/124084.jpg")
 # im = AiA.imread("probabilistic_data/134052.jpg")
 # im = plt.imread("probabilistic_data/164074.jpg")
 # im = AiA.imread("probabilistic_data/Mindre_isbjorn.jpg", as_type=True,load_type=np.uint8)
@@ -26,7 +26,7 @@ im = plt.imread("probabilistic_data/124084.jpg")
 # im = im*255
 # AiA.imshow(im)
 
-snake = snek.snake(300, im,
+snake = snek.snake(150, im,
                    tau=1.5,
                    var_tau=False,
                    alpha=0.001, 
